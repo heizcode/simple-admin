101501baf9253393709f0d5870a0cd699e3bc9f9
zhulx <>
2023-06-09 16:35:27 +0800

    【改进】 - 修复公共模块自调用问题、 【改进】 - 打jar包的时候缺失BOOT-INT 问题 【新增】 - 全局Lombok依赖

---
+++ simple-admin-app/simple-module/simple-admin-sys/src/main/java/com/simple/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.simple.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhulx
+ * @version 1.0
+ * @ClassName TestController
+ * @Description TODO
+ * @Date 2023/6/9 14:45
+ * @since JDK1.8
+ */
+@RestController
+@RequestMapping("sys/test")
+public class TestController {
+
+    @GetMapping(value = "/getTestInfo")
+    public String getUserInfo() {
+        return "这是测试接口返回的测试文字";
+    }
+
+}
